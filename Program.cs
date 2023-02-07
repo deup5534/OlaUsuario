a4df863af67751696a9d5f740891cd596928d107
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-02-07 18:20:26 -0300

    daora

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+//parte do "robô"
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Oii! Me diga seu nome para nos conhecermos!^^");
+Console.ResetColor();
+
+//parte do usuário
+string nomezin = Console.ReadLine()!;
+
+//parte do "robô"
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"Olá {nomezin}!");
+Console.ResetColor();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OlaUsuario
 07/02/2023 - Voltando á programar
+
+# Enunciado
+Faça um programa solicite que o usuário digite seu nome. Exiba Olá, ___! (na lacuna ___ exiba o nome digitado). 
